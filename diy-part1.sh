a504ef5ef6e73609d02d8ed03cee681d819ae096
hanimeia <85946958+hanimeia@users.noreply.github.com>
2025-05-17 19:46:48 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -10,9 +10,9 @@
 # See /LICENSE for more information.
 #
 
-# Uncomment a feed source
-#sed -i 's/^#\(.*openclash\)/\1/' feeds.conf.default
+Uncomment a feed source
+sed -i 's/^#\(.*openclash\)/\1/' feeds.conf.default
 
-# Add a feed source
-#echo 'src-git openclash https://github.com/vernesong/OpenClash' >>feeds.conf.default
+Add a feed source
+echo 'src-git openclash https://github.com/vernesong/OpenClash' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
